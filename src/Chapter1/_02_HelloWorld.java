372d508acbafe1805ab6d4d817a131596c1611f6
qifu <fuqiqi12345@gmail.com>
2017-05-25 22:39:18 -0400

    Exercise 2

--- src/Chapter1/_02_HelloWorld.java
+++ src/Chapter1/_02_HelloWorld.java
@@ -3,7 +3,7 @@ package Chapter1;
 /**
  * Created by qifu on 17/5/25.
  */
-public class HelloWorld {
+public class _02_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World");
     }
