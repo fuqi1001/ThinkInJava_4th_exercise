abedd654a100b10a882968c649d3bd39d0e8b2ec
qifu <fuqiqi12345@gmail.com>
2017-05-25 23:25:36 -0400

    Exercise 10

---
+++ src/Chapter1/_09_CommandArgTest.java
@@ -0,0 +1,12 @@
+package Chapter1;
+
+/**
+ * Created by qifu on 17/5/25.
+ */
+public class _09_CommandArgTest {
+    public static void main(String[] args) {
+        System.out.println("args[0] = " + args[0]);
+        System.out.println("args[1] = " + args[1]);
+        System.out.println("args[2] = " + args[2]);
+    }
+}
