dcd85c7213b995754cbbcaeb7097fb779bdde13d
qifu <fuqiqi12345@gmail.com>
2017-05-25 22:33:57 -0400

    Exercise 2

---
+++ src/Everything/is/an/Object/HelloWorld.java
@@ -0,0 +1,14 @@
+package Everything.is.an.Object;
+
+/**
+ * Created by qifu on 17/5/25.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
+/*
+Output:
+Hello, World
+ */
