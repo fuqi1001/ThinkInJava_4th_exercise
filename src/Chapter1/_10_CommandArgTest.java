90bda8af80d0c77813a42fb6bf03192fb5a05cea
qifu <fuqiqi12345@gmail.com>
2017-05-25 23:25:52 -0400

    Exercise 10

--- src/Chapter1/_10_CommandArgTest.java
+++ src/Chapter1/_10_CommandArgTest.java
@@ -3,7 +3,7 @@ package Chapter1;
 /**
  * Created by qifu on 17/5/25.
  */
-public class _09_CommandArgTest {
+public class _10_CommandArgTest {
     public static void main(String[] args) {
         System.out.println("args[0] = " + args[0]);
         System.out.println("args[1] = " + args[1]);
