947f5c10ddd96a2db8a33f3c0edb7ecbb33c3b14
qifu <fuqiqi12345@gmail.com>
2017-05-26 22:56:27 -0400

    Exercise 1

--- src/Chapter2/_01_PrintTest.java
+++ src/Chapter2/_01_PrintTest.java
@@ -5,13 +5,12 @@ package Chapter2;
  */
 // Write a program that uses the "short" and long form of print statement
 
-import static net.mindview.util.Print.*;
-import org.greggordon.tools.*;
+
 
 public class _01_PrintTest {
     public static void main(String[] args) {
-        print("Hello, from short form.");
-        P.rintln("Hello from greggordon form.");
+        System.out.println("Hello, from short form.");
+        System.out.println("Hello from greggordon form.");
         System.out.println("Hello from long form.");
     }
 }
