d77a7d8d6276e0e1c452335e10b8541629d7a492
qifu <fuqiqi12345@gmail.com>
2017-05-27 00:02:24 -0400

    Exercise 12

---
+++ src/Chapter2/_12_RightShiftTest2.java
@@ -0,0 +1,58 @@
+package Chapter2;
+
+/**
+ * Created by qifu on 17/5/27.
+ */
+/* Start with a number that is all binary ones. Left shift it, then use the
+* unsigned right-shift operator to right shift through all of its binary
+* positions, each time displaying the result using Integer.toBinarySting().
+*/
+public class _12_RightShiftTest2 {
+    public static void main(String [] args) {
+        int h = -1;
+        System.out.println(Integer.toBinaryString(h));
+        h <<= 10;
+        System.out.println(Integer.toBinaryString(h));
+        for(int i = 0; i < 32; i++) {
+            h >>>= 1;
+            System.out.println(Integer.toBinaryString(h));
+        }
+    }
+}
+/*
+output:
+11111111111111111111111111111111
+11111111111111111111110000000000
+1111111111111111111111000000000
+111111111111111111111100000000
+11111111111111111111110000000
+1111111111111111111111000000
+111111111111111111111100000
+11111111111111111111110000
+1111111111111111111111000
+111111111111111111111100
+11111111111111111111110
+1111111111111111111111
+111111111111111111111
+11111111111111111111
+1111111111111111111
+111111111111111111
+11111111111111111
+1111111111111111
+111111111111111
+11111111111111
+1111111111111
+111111111111
+11111111111
+1111111111
+111111111
+11111111
+1111111
+111111
+11111
+1111
+111
+11
+1
+0
+ */
