94b144ba2c1322323600153766bd51777b8039d8
qifu <fuqiqi12345@gmail.com>
2017-07-01 21:08:23 -0400

    Exercise 1

---
+++ src/Chapter4/ConstructorTest.java
@@ -0,0 +1,14 @@
+package Chapter4;
+
+/**
+ * Created by qifu on 17/7/1.
+ */
+class Tester{
+    String s;
+}
+public class ConstructorTest {
+    public void main(String[] args) {
+        Tester t = new Tester();
+        System.out.println(t.s);
+    }
+}
