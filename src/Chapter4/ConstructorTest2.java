8a27d1f7ebb939ab5602ca5a609e87e284f30137
qifu <fuqiqi12345@gmail.com>
2017-07-01 21:10:53 -0400

    Exercise 2

---
+++ src/Chapter4/ConstructorTest2.java
@@ -0,0 +1,13 @@
+package Chapter4;
+
+/**
+ * Created by qifu on 17/7/1.
+ */
+public class ConstructorTest2 {
+    public static void main(String[] args) {
+        Tester2 t = new Tester2();
+        System.out.println("t.s1: " + t.s1);
+        System.out.println("t.s2: " + t.s2);
+        System.out.println("t.s3: " + t.s3);
+    }
+}
---
+++ src/Chapter4/Tester2.java
@@ -0,0 +1,13 @@
+package Chapter4;
+
+/**
+ * Created by qifu on 17/7/1.
+ */
+public class Tester2 {
+    String s1 ;
+    String s2 = "hello";
+    String s3 ;
+    Tester2() {
+        s3 = "goodbye";
+    }
+}
