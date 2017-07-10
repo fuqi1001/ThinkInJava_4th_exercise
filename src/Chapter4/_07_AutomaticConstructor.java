9af36449141c70e9541c4f9fdb12d00d11427160
qifu <fuqiqi12345@gmail.com>
2017-07-10 12:07:52 -0400

    Exercise 7

---
+++ src/Chapter4/_07_AutomaticConstructor.java
@@ -0,0 +1,14 @@
+package Chapter4;
+
+/**
+ * Created by qifu on 17/7/10.
+ */
+public class _07_AutomaticConstructor {
+    public static void main(String[] args) {
+        Aso a = new Aso();
+        a.bark();
+    }
+}
+class Aso {
+    void bark() {System.out.println("woof");}
+}
