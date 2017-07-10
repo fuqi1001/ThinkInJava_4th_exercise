0e2f32cbac1b0e5040f26e1935e456d7c4bd424e
qifu <fuqiqi12345@gmail.com>
2017-07-10 12:00:33 -0400

    Exercise 3

---
+++ src/Chapter4/_03_DefaultConstructorTest.java
@@ -0,0 +1,16 @@
+package Chapter4;
+
+/**
+ * Created by qifu on 17/7/10.
+ */
+public class _03_DefaultConstructorTest {
+    public static void main(String[] args) {
+        Kabayo kabayo = new Kabayo();
+    }
+}
+class Kabayo {
+    Kabayo() {
+        System.out.println("This is a kabayo");
+    }
+}
+
