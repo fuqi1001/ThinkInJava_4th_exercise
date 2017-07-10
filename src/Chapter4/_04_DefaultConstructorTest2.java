57cc7008522a491b19384f5c5418cc5518a901cf
qifu <fuqiqi12345@gmail.com>
2017-07-10 12:03:56 -0400

    Exercise 4

---
+++ src/Chapter4/_04_DefaultConstructorTest2.java
@@ -0,0 +1,19 @@
+package Chapter4;
+
+/**
+ * Created by qifu on 17/7/10.
+ */
+public class _04_DefaultConstructorTest2 {
+    public static void main(String[] args){
+        Kabayo2 ka = new Kabayo2();
+        Kabayo2 kaa = new Kabayo2("123");
+    }
+}
+class Kabayo2 {
+    Kabayo2() {
+        System.out.println("this is the kabayo2");
+    }
+    Kabayo2(String s) {
+        System.out.println("Reloading constructor");
+    }
+}
