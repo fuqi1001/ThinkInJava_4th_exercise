e1bc7e4a811865a65f3b48fc20081031a8fa61be
qifu <fuqiqi12345@gmail.com>
2017-07-13 13:09:54 -0400

    Exercise 11

---
+++ src/Chapter8/_11_Sandwich.java
@@ -0,0 +1,54 @@
+package Chapter8;
+
+/**
+ * Created by qifu on 17/7/13.
+ */
+class Meal {
+    Meal() {
+        System.out.println("Super class, Meal()");
+    }
+}
+class Bread {
+    Bread() {
+        System.out.println("Super class, Bread()");
+    }
+}
+class Cheese {
+    Cheese() {
+        System.out.println("Super class, Cheese()");
+    }
+}
+
+class Lettuce {
+    Lettuce() {
+        System.out.println("Super class, Lettuce()");
+    }
+}
+class Pickle {
+    Pickle() {
+        System.out.println("Super class, Pickle()");
+    }
+}
+
+class Lunch extends Meal {
+    Lunch() {
+        System.out.println("Drived class, Lunch()");
+    }
+}
+class PortableLunch extends Lunch {
+    PortableLunch() {
+        System.out.println("Drived class, Port");
+    }
+}
+public class _11_Sandwich extends PortableLunch{
+    private Bread b = new Bread();
+    private Cheese c = new Cheese();
+    private Pickle p = new Pickle();
+    private Lettuce l = new Lettuce();
+    private Sandwich() {
+        System.out.println("Create Sandwich");
+    }
+    public static void main(String[] args) {
+        new Sandwich();
+    }
+}
